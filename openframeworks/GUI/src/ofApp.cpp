b8826ef3024a99ccb67a3b830d51be6e91ef859c
Robotic-Animations <mathwhiz101@gmail.com>
2016-12-06 16:55:04 -0900

    committing refactor

--- openframeworks/GUI/src/ofApp.cpp
+++ openframeworks/GUI/src/ofApp.cpp
@@ -1,80 +1,79 @@
 #include "ofApp.h"
 
 void slider(const string & name, ofxGuiGroup * panel) {
-	ofParameter<float> fk;
-	ofxGuiContainer *buttons;
-	panel->add(fk.set(name, 50, 0, 360), ofJson({ { "precision",2 } }));//these will access the position of the stepper motors
-	buttons = panel->addContainer("", ofJson({ { "direction","horizontal" } }));//adds buttons to fine tune arm position
-	buttons->add<ofxGuiButton>("Less", ofJson({ { "type","fullsize" },{ "width",ofGetWidth()*.1 },{ "text-align","center" } })); //access panel width
+	ofParameter<float> parameter;
+	panel->add(parameter.set(name, 50, 0, 360), ofJson({ { "precision",2 } }));//these will access the position of the stepper motors
+	ofxGuiContainer *buttons = panel->addContainer("", ofJson({ { "direction","horizontal" } }));//adds buttons to fine tune arm position
+	buttons->add<ofxGuiButton>("Less", ofJson({ { "type","fullsize" },{ "width",ofGetWidth()*.1 },{ "text-align","center" } }));
 	buttons->add<ofxGuiButton>("More", ofJson({ { "type","fullsize" },{ "width",ofGetWidth()*.1 },{ "text-align","center" } }));
 }
 
-void draw_image(ofxGuiContainer * panel, const string & name, const float & x, const float & y, ofImage & picture) {
+template <typename T>
+void draw_image(T * panel, const string & name, const float & x, const float & y, ofImage & picture) {
 	panel->add<ofxGuiGraphics>(name, &picture.getTexture(), ofJson({ { "width", ofGetWindowWidth()*x },{ "height", ofGetWindowHeight()*y } }));
 }
 
 void gauge(ofxGuiContainer * container, const string & name) {
 	ofParameter<float> temp;
-	container->add(temp.set(name, ofGetFrameRate() + 10, 0, 85), ofJson({ { "precision",2 },{ "type", "circular" },{ "width",140 },{ "height",100 } }));//replace max with actual motor max value
+	container->add(temp.set(name, ofGetFrameRate() + 10, 0, 85), ofJson({ { "precision",2 },{ "type", "circular" },{ "width",140 },{ "height",100 } }));
 }
 
-void power_gauge(ofxGuiContainer * container, const string & name) {
+void power_gauge(ofxGuiContainer * container, const string & name, const int & width=0) {
 	ofParameter<float> temp;
-	container->add(temp.set(name, 80, 0, 100), ofJson({ { "precision",2 },{ "width",40 },{ "height",100 } }));//access the battery levels
-}
-
-void alt_power_gauge(ofxGuiContainer * container, const string & name) {
-	ofParameter<float> temp;
-	container->add(temp.set(name, 80, 0, 100), ofJson({ { "precision",2 }/*,{ "width",40 },{ "height",100 }*/ }));//access the battery levels
-
-	//ofParameter<int> temp;
-	//container->add(temp.set(name, 70, 0, 100));
+	container->add(temp.set(name, 80, 0, 100), ofJson({ { "precision",2 },{ "width",width } }));
 }
 
 ofxGuiPanel * gui_panel(ofxGui & gui, const string & name, const float & width) {
 	return gui.addPanel(name, ofJson({ { "width",ofGetWidth()*width } }));
 }
 
-ofxGuiPanel * gui_panel(ofxGui & gui, const string & name, const float & width, const float & height) {
-	return gui.addPanel(name, ofJson({ {"width",ofGetWidth()*width}, {"height",ofGetWidth()*height} }));
-}
-
-ofxGuiPanel * gui_panel_debug(ofxGui & gui, const string & name) {
-	return gui.addPanel(name, ofJson({ { "width",100 } }));
-}
-
-struct panel_info {
-	panel_info() {}
-	panel_info(const float & width) : width(width), height(0) {}
-	panel_info(const float & width, const float & height) : width(width), height(height) {}
+struct panel_dimensions {
+	panel_dimensions() {}
+	panel_dimensions(const float & width) : width(width), height(0) {}
+	panel_dimensions(const float & width, const float & height) : width(width), height(height) {}
 	float width;
-	float height;
+	float height; //unused
 };
-map<string, panel_info> panels_info;
+std::pair<string, panel_dimensions> panel_pairer(const string & name, const float & width, const float & height=0) {
+	return std::pair<string, panel_dimensions>(name, panel_dimensions(width, height));
+}
 
+//Globals, used in setup and update or callbacks. Maybe moved into the class?
+map<string, panel_dimensions> panels_info;
 float aspect_ratio;
 
 //--------------------------------------------------------------
 void ofApp::setup(){
 	picture = ofImage("images/Sun.jpg");
+	hand.load("images/Hand Image.jpg");
 	aspect_ratio = picture.getHeight() / picture.getWidth();
-
 	ofSetFrameRate(60);
 
-	panels_info = map<string, panel_info> {
-		make_pair<string, panel_info>("Arm Controls", panel_info(.2)),
-		make_pair<string, panel_info>("Cameras", panel_info(.5, .5*aspect_ratio)),
-		make_pair<string, panel_info>("Motor Feedback", panel_info(.55)),
-		make_pair<string, panel_info>("Hand Controls", panel_info(.2))
+	panels_info = map<string, panel_dimensions>{
+		panel_pairer("Arm Controls", .2),
+		panel_pairer("Cameras", .5, .5*aspect_ratio),
+		panel_pairer("Motor Feedback", .55),
+		panel_pairer("Hand Controls", .2),
 	};
-	for (auto panel : panels_info) {
-		if (panel.second.height) panels[panel.first] = gui_panel(gui, panel.first, panel.second.width, panel.second.height);
-		else panels[panel.first] = gui_panel(gui, panel.first, panel.second.width);
+	for (auto panel : panels_info) { //mapping the panels info to the panels in the class, ofxGuiPanels
+		panels[panel.first] = gui_panel(gui, panel.first, panel.second.width);
 	}
 
-	//arm
-	arm_FK = panels["Arm Controls"]->addGroup("Forward Kinematics");//a collapsable group of sliders controlling forward kinematics
-	arm_IK = panels["Arm Controls"]->addGroup("Inverse Kinematics");//a collapsible group of sliders controlling inverse kinematics
+	//Panel Initialization
+	arm_FK = panels["Arm Controls"]->addGroup("Forward Kinematics");
+	arm_IK = panels["Arm Controls"]->addGroup("Inverse Kinematics");
+	main_Camera = panels["Cameras"]->addContainer("Main Camera");
+	other_Cameras = panels["Cameras"]->addContainer("Other Cameras", ofJson({ { "direction","horizontal" } }));
+	motor_Container = panels["Motor Feedback"]->addContainer("Motors", ofJson({ { "direction","horizontal" } }));
+	hand_image = panels["Hand Controls"]->addGroup("3D Model");
+	hand_control = panels["Hand Controls"]->addGroup("Hand Controls");
+
+	//Positioning
+	panels["Cameras"]->setPosition(panels["Arm Controls"]->getShape().getTopRight() + ofPoint(20, 0));
+	panels["Motor Feedback"]->setPosition(panels["Cameras"]->getShape().getBottomLeft() + ofPoint(0, 10));
+	panels["Hand Controls"]->setPosition(panels["Cameras"]->getShape().getTopRight() + ofPoint(20, 0));
+
+	//Arms
 	slider("X-Axis", arm_FK);
 	slider("Y-Axis", arm_FK);
 	slider("Z-Axis", arm_FK);
@@ -83,36 +82,23 @@ void ofApp::setup(){
 	slider("Z-Axis", arm_IK);
 	panels["Arm Controls"]->addFpsPlotter();
 
-	//main camera
-	panels["Cameras"]->setPosition(panels["Arm Controls"]->getShape().getTopRight() + ofPoint(20,0));
-	main_Camera = panels["Cameras"]->addContainer("");
+	//Cameras
 	draw_image(main_Camera, "Camera 1",  panels_info["Cameras"].width, panels_info["Cameras"].width*aspect_ratio, picture);
-
-	other_Cameras = panels["Cameras"]->addContainer("", ofJson({ { "direction","horizontal" } }));
 	draw_image(other_Cameras, "Camera 2", panels_info["Cameras"].width/3, panels_info["Cameras"].width*aspect_ratio/3, picture);
 	draw_image(other_Cameras, "Camera 3", panels_info["Cameras"].width/3, panels_info["Cameras"].width*aspect_ratio/3, picture);
 	draw_image(other_Cameras, "Depth Camera", panels_info["Cameras"].width/3, panels_info["Cameras"].width*aspect_ratio/3, picture);
 
-	//motor feedback
-	panels["Motor Feedback"]->setPosition(panels["Cameras"]->getShape().getBottomLeft() + ofPoint(0,10));
-	motor_Container = panels["Motor Feedback"]->addContainer("motors", ofJson({ {"direction","horizontal"} }));
-
+	//Motors
 	gauge(motor_Container, "Left Front");
 	gauge(motor_Container, "Right Front");
 	gauge(motor_Container, "Left Rear");
 	gauge(motor_Container, "Right Rear");
-	power_gauge(motor_Container, "Battery Level");
+	power_gauge(motor_Container, "Battery Level", 40);
 
-	//hand control
-	hand.load("images/Hand Image.jpg");
-
-	panels["Hand Controls"]->setPosition(panels["Cameras"]->getShape().getTopRight() + ofPoint(20, 0));
-
-	hand_image = panels["Hand Controls"]->addGroup("3D Model");
-	hand_image->add<ofxGuiGraphics>("Hand Model", &hand.getTexture(), ofJson({ { "width", ofGetWindowWidth()*.18 },{ "height", ofGetWindowHeight()*.13 } }));
-	hand_control = panels["Hand Controls"]->addGroup("Hand Controls");
+	//Hand control
+	draw_image(hand_image, "Hand Model", .18f, .13f, hand);
 	gauge(hand_control, "Rotation");
-	alt_power_gauge(hand_control, "Clamp");
+	power_gauge(hand_control, "Clamp", 200);
 }
 
 
@@ -174,15 +160,22 @@ void ofApp::mouseExited(int x, int y){
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-	panels["Arm Controls"]->setWidth(ofGetWidth()*panels_info["Arm Controls"].width);
+	panels["Arm Controls"]->setWidth(w*panels_info["Arm Controls"].width);
+	panels["Motor Feedback"]->setWidth(w*panels_info["Motor Feedback"].width);
+	panels["Hand Controls"]->setWidth(w*panels_info["Hand Controls"].width);
 
-	panels["Cameras"]->setWidth(ofGetWidth()*panels_info["Cameras"].width);
-	panels["Cameras"]->setHeight(ofGetWidth()*panels_info["Cameras"].width*aspect_ratio*.75);
+	panels["Cameras"]->setWidth(w*panels_info["Cameras"].width);
+	panels["Cameras"]->setHeight(w*panels_info["Cameras"].width*aspect_ratio*.75);
+	
+	main_Camera->setWidth(w*panels_info["Cameras"].width);
+	main_Camera->setHeight(w*panels_info["Cameras"].width*aspect_ratio*.75);
+	
+	//figure out how ot resize image, first drawn in "draw_image"
+	//panel->add<ofxGuiGraphics>(name, &picture.getTexture(), ofJson({ { "width", ofGetWindowWidth()*x },{ "height", ofGetWindowHeight()*y } }));
+	//ofxGuiGraphics temp = panels["Cameras"]->findChildrenById("Main Camera")[0]->findChildrenById("Camera 1")[0]->setWidth(w*panels_info["Cameras"].width);
+	//docs don't help... good luck
+
 	panels["Cameras"]->setPosition(panels["Arm Controls"]->getShape().getTopRight() + ofPoint(20, 0));
-	main_Camera->setWidth(ofGetWidth()*panels_info["Cameras"].width);
-	main_Camera->setHeight(ofGetWidth()*panels_info["Cameras"].width*aspect_ratio*.75);
-	//figfure out how ot resize image, first drawn in "draw_image"
-
 	panels["Motor Feedback"]->setPosition(panels["Cameras"]->getShape().getBottomLeft() + ofPoint(0, 10));
 	panels["Hand Controls"]->setPosition(panels["Cameras"]->getShape().getTopRight() + ofPoint(20, 0));
 }
--- openframeworks/GUI/src/ofApp.h
+++ openframeworks/GUI/src/ofApp.h
@@ -1,37 +1,27 @@
-#pragma once
-
 #include "ofMain.h"
 #include "ofxGuiExtended.h"
-//#include "ofxMasterSlaveControl.h"
 
 class ofApp : public ofBaseApp{
+public:
+	void setup();
+	void update();
+	void draw();
 
-	public:
-		void setup();
-		void update();
-		void draw();
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y );
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
-
-		ofxGui gui;
-
-		map<string, ofxGuiPanel*> panels;
-		ofxGuiGroup* arm_FK, *arm_IK;
-		ofxGuiContainer* main_Camera, *other_Cameras;
-		ofxGuiGroup* hand_control, *hand_image;
-		ofxGuiContainer* motor_Container;
-
-		ofImage hand;
-		ofImage picture;
+	map<string, ofxGuiPanel*> panels;
+	ofxGui gui;
+	ofxGuiGroup *arm_FK, *arm_IK, *hand_control, *hand_image;
+	ofxGuiContainer *main_Camera, *other_Cameras, *motor_Container;
+	ofImage hand, picture;
 };
